0d8d54dceb6940a3a99f3e0827def129dc33ef91
Daniil <66374958+DanilinStati@users.noreply.github.com>
2023-09-12 09:32:07 +0300

    Add files via upload

---
+++ SwiftLeetCode/SwiftLeetCode/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftLeetCode
+//
+//  Created by Daniil on 12.09.2023.
+//
+
+import Foundation
+
+class Solution {
+    func foo() {
+        
+    }
+}
+
+let soution = Solution()
+print(soution.foo())
